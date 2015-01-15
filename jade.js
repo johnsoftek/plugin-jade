957fbf3e4e670b2f41ad2066fdd8bc9049ba3531
johnsoftek <john.softek@gmail.com>
2015-01-16 00:27:39 +1000

    Simplify require of runtime.js

--- jade.js
+++ jade.js
@@ -1,10 +1,8 @@
 var Jade = require('./jade-compiler.min');
 
-exports.runtime = require('./jade-runtime');
-
 exports.translate = function(load) {
 	var fn = Jade.compileClient(load.source);
-  var runtime = "var jade = require('jade').runtime;\n\n";
+  var runtime = "var jade = require('./jade-runtime.min');\n\n";
 	return runtime + 'module.exports = ' + fn;
 };
 
