b460caa79aae54d27f91aa41e907cd9274e19d06
johnsoftek <john.softek@gmail.com>
2015-01-19 13:21:56 +1000

    Npm version

--- jade.js
+++ jade.js
@@ -1,8 +1,10 @@
-var Jade = require('./jade-compiler.min');
+var Jade = require('jade');
+
+exports.runtime = require('runtime');
 
 exports.translate = function(load) {
 	var fn = Jade.compileClient(load.source);
-  var runtime = "var jade = require('./jade-runtime.min');\n\n";
+  var runtime = "var jade = require('jade').runtime;\n\n";
 	return runtime + 'module.exports = ' + fn;
 };
 
