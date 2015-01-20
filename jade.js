b46b3f8f0ddedbf4f30b8dfec9d53404b5c4c718
johnsoftek <john.softek@gmail.com>
2015-01-20 20:42:46 +1000

    Test github variation

--- jade.js
+++ jade.js
@@ -1,13 +1,10 @@
-console.log('here')
-var Jade = require('jade-compiler');
-console.log('here2')
+var compiler = require('jade-compiler');
 
 exports.runtime = require('jade-compiler/runtime');
 
 exports.translate = function(load) {
-console.log('here3')
-	var fn = Jade.compileClient(load.source);
+  var fn = compiler.compileClient(load.source);
   var runtime = "var jade = require('jade').runtime;\n\n";
-	return runtime + 'module.exports = ' + fn;
+  return runtime + 'module.exports = ' + fn;
 };
 
