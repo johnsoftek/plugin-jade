91a139a7a228057f4e5b849d88a4f8e6d5217500
johnsoftek <john.softek@gmail.com>
2015-01-20 19:30:11 +1000

    fetched Jade compiler and runtime from github

--- jade.js
+++ jade.js
@@ -1,8 +1,11 @@
-var Jade = require('jade');
+console.log('here')
+var Jade = require('jade-compiler');
+console.log('here2')
 
-exports.runtime = require('runtime');
+exports.runtime = require('jade-compiler/runtime');
 
 exports.translate = function(load) {
+console.log('here3')
 	var fn = Jade.compileClient(load.source);
   var runtime = "var jade = require('jade').runtime;\n\n";
 	return runtime + 'module.exports = ' + fn;
