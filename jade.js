a576777a3bb2a229cdd405a6ac68b87309afda00
Jiří Špác <capajj@gmail.com>
2015-06-16 14:35:16 +0200

    updated jade and fixed a problem with normalization(probably JSPM systemjs changed the API since the last time this plugin tests were run)

--- jade.js
+++ jade.js
@@ -1,17 +1,12 @@
+/* */
+"format cjs";
 var Jade = require('jade-compiler');
-var P_jade_loc = System.normalize('jade-compiler', module.id );
+var P_jade_loc = 'npm:jade@1.11.0/runtime';
 
 exports.translate = function(load) {
-  var template_fn = Jade.compileClient(load.source);
-  return Promise.resolve(
-    P_jade_loc.then(function(jade_loc) {
-      // Note: mask "require" by separating from  left parenthesis to prevent
-      // dependency processing on module load.
-      //
-      return "var jade = require" + "('" + jade_loc + "/lib/runtime');\n\n" +
-        'module.exports = ' + template_fn;
-    })
-  );
-
+	var template_fn = Jade.compileClient(load.source);
+	return "var jade = require" + "('" + P_jade_loc + "');\n\n" + 'module.exports = ' + template_fn;
 };
+
+
 
