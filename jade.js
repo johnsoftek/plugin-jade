bbd617c7d23aff764aea5cb437badf6ffd6a44ef
johnsoftek <john.softek@gmail.com>
2015-08-02 22:31:44 +1000

    Fix error locating Jade runtime under jspm 0.16.0-beta.3

--- jade.js
+++ jade.js
@@ -1,14 +1,14 @@
 var Jade = require('jade-compiler');
-var P_jade_loc = System.normalize('jade-compiler', module.id );
+var P_runtime_loc = System.normalize('jade-compiler/lib/runtime', module.id );
 
 exports.translate = function(load) {
   var template_fn = Jade.compileClient(load.source);
   return Promise.resolve(
-    P_jade_loc.then(function(jade_loc) {
-      // Note: mask "require" by separating from  left parenthesis to prevent
+		P_runtime_loc.then(function(runtime_loc) {
+			// Note: mask "require" by separating from  left parenthesis to prevent
       // dependency processing on module load.
       //
-      return "var jade = require" + "('" + jade_loc + "/lib/runtime');\n\n" +
+      return "var jade = require" + "('" + runtime_loc + "');\n\n" +
         'module.exports = ' + template_fn;
     })
   );
